c7025682fd361511eab1d9e834a1387858c1969c
KevinNuness <164961450+KevinNuness@users.noreply.github.com>
2025-03-02 13:10:32 -0300

    Create 1067.java

---
+++ Java/1067_NúmerosÍmpares/1067.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int x;
+		boolean verificador = false;
+		
+		do {
+		    
+		    x = sc.nextInt();
+		    
+		    if (x >= 1 && x <= 1000){
+		     verificador = true;   
+		    }
+		} while (verificador == false);
+		
+		for (int i = 0; i <= x; i++){
+		    if(i % 2 != 0){
+		System.out.println( i );
+		}
+	  }
+	}
+}
